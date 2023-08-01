2276f5e88e668e95e9210877fa205226bbaa32c8
tcxone <tcxone@outlook.com>
2023-08-01 17:12:31 +0800

    v0.0.1

--- main.go
+++ main.go
@@ -4,18 +4,16 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"mime"
 	"net/http"
 	"os"
 	"path/filepath"
-	"time"
 )
 
 func loggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
 		log.Printf("[%s] %s %s %s", "INFO", r.RemoteAddr, r.Method, r.URL)
 		next.ServeHTTP(w, r)
-		log.Printf("[%s] %s %s", "INFO", "Done in", time.Since(start))
 	})
 }
 
@@ -35,14 +33,35 @@ func main() {
 			}
 			fmt.Fprintln(w, "<html lang='en'><head><meta charset='utf-8'><title>Directory listing for "+r.URL.Path+"</title></head><body><h1>Directory listing for "+r.URL.Path+"</h1><hr><ul>")
 			for _, name := range list {
-				fmt.Fprintf(w, "<li><a href=\"/%s\">%s</a></li>\n", filepath.Join(path, name), name)
+				nameInfo, nameErr := os.Stat(path + "/" + name)
+				if nameErr != nil {
+				}
+				if nameInfo.IsDir() {
+					fmt.Fprintf(w, "<li><a href=\"/%s\">%s/</a></li>\n", filepath.Join(path, name), name)
+				} else {
+					fmt.Fprintf(w, "<li><a href=\"/%s\">%s</a></li>\n", filepath.Join(path, name), name)
+				}
 			}
 			fmt.Fprintln(w, "</ul><hr></body></html>")
 		} else {
+			mimeType := mime.TypeByExtension(filepath.Ext(path))
+
+			if mimeType == "" {
+				mimeType = "application/octet-stream"
+			}
+
+			w.Header().Set("Content-Type", mimeType)
+
 			content, _ := ioutil.ReadFile(path)
 			fmt.Fprintf(w, "%s", content)
 		}
 	})
-	log.Println("Start server on 0.0.0.0:8088")
-	http.ListenAndServe(":8088", loggingMiddleware(http.DefaultServeMux))
+	var port int
+	fmt.Print("input server port >>>")
+	fmt.Scanln(&port)
+	/*if len(os.Args)>1{
+		port = os.Args[len(os.Args)-1]
+	}*/
+	log.Println("[INFO] Start server on 0.0.0.0:" + fmt.Sprintf("%d", port))
+	http.ListenAndServe(fmt.Sprintf(":%d", port), loggingMiddleware(http.DefaultServeMux))
 }
